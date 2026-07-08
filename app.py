bb2ce05b813b34eeaac2322bff36ae55749cc33a
hamyeju432-hue <hamyeju432@gmail.com>
2026-07-09 07:15:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import streamlit as st
 from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
-st.title("🏥 전국 종합병원 안전 의료 지도")
-st.markdown("깃허브에 저장된 실시간 데이터를 바탕으로 지도를 생성합니다.")
 
 Korea_Map = folium.Map(location=[36.00, 127.25], zoom_start=7)
 
@@ -48,7 +46,5 @@ while hp:
     hp = response.readline().decode("utf-8")
 
 response.close()
-
-st.subheader("📍 지도 시각화 결과")
 
 st_folium(Korea_Map, use_container_width=True, height=650)
